f7224f018ab899b711147ac1eb909ef06179ccdf
abishop0894 <abishop0894@gmail.com>
2025-03-07 14:02:01 -0500

    new

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Page from "@/app/modules/components/layout/Page";
 import Hero from "@/app/modules/components/layout/Hero";
 export default function Component() {
   return (
+    
     <Page>
      <Hero />
     </Page>
